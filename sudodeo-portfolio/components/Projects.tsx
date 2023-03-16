5829424d39a331194dab55b8cbdf3d5187364f81
mikelisrael <israelipinkz@gmail.com>
2023-03-16 20:47:29 +0100

    feat: commence project section

---
+++ sudodeo-portfolio/components/Projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="universal_x universal_y">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nostrum
+      rerum amet quasi porro eos similique perspiciatis consequatur odit commodi
+      alias rem vero sequi, at, quis deserunt ipsum ipsam tempora!
+    </div>
+  );
+};
+
+export default Projects;
