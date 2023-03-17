5091c1a2bc67f3824e8b84e87900266123c05837
mikelisrael <israelipinkz@gmail.com>
2023-03-17 23:56:12 +0100

    style: add images to projects

--- sudodeo-portfolio/components/Projects.tsx
+++ sudodeo-portfolio/components/Projects.tsx
@@ -1,36 +1,74 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
+
+interface ProjectsFields {
+  first_column: {
+    title: string;
+    description?: string;
+    image?: string;
+  }[];
+  second_column: {
+    title: string;
+    description?: string;
+    image?: string;
+  }[];
+}
+
+const projects: ProjectsFields = {
+  first_column: [
+    {
+      title: "social media API",
+      description: "A basic social media API built with FastAPI",
+      image: "./social.jpg",
+    },
+    {
+      title: "gizmo",
+      description: "friendly discord bot",
+      image: "./gizmo.jpg",
+    },
+  ],
+  second_column: [
+    {
+      title: "storeX",
+      description: "Robust e-commerce Api",
+      image: "./storex.jpg",
+    },
+    {
+      title: "tech news API",
+      description:
+        "A scraper that gets trending tech content from different news sites and serves them via an API",
+      image: "./technews.jpg",
+    },
+  ],
+};
 
 const Projects = () => {
   return (
     <section
-      className="universal_x universal_y grid sm:grid-cols-2 gap-[2px]"
+      className="projects universal_x universal_y grid sm:grid-cols-2 gap-[3px]"
       id="projects"
     >
-      <div className="space-y-[2px]">
+      <div className="space-y-[3px]">
         <div className="mb-16">
           <h2 className="title !mb-3">Projects</h2>
           <em>At least a few...</em>
         </div>
 
-        <article
-          data-aos="fade-up"
-          className="bg-teal-300 h-72 md:h-96 lg:h-[30rem]"
-        ></article>
-        <article
-          data-aos="fade-up"
-          className="bg-cyan-300 h-72 md:h-96 lg:h-[30rem]"
-        ></article>
+        {
+          // @ts-ignore
+          projects.first_column.map((item) => (
+            <ProjectCard {...item} />
+          ))
+        }
       </div>
 
-      <div className="space-y-[2px]">
-        <article
-          data-aos="fade-up"
-          className="bg-orange-300 h-72 md:h-96 lg:h-[30rem]"
-        ></article>
-        <article
-          data-aos="fade-up"
-          className="bg-red-300 h-72 md:h-96 lg:h-[30rem]"
-        ></article>
+      <div className="space-y-[3px]">
+        {
+          // @ts-ignore
+          projects.second_column.map((item) => (
+            <ProjectCard {...item} />
+          ))
+        }
       </div>
     </section>
   );
