b36535754725b2c2a1c2caaad3f2f2babbc30844
mikelisrael <israelipinkz@gmail.com>
2023-03-19 04:27:31 +0100

    layout: change loader rendering style to improve SEO

--- sudodeo-portfolio/components/LoadingScreen.tsx
+++ sudodeo-portfolio/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Animations.module.css";
 const LoadingScreen = () => {
   return (
     <div
-      className={`relative h-screen w-full flex items-center justify-center scale-50  lg:scale-100 -translate-y-5 lg:translate-y-0 ${styles.loader}`}
+      className={`fixed top-0 left-0 bottom-0 h-screen w-full flex items-center justify-center scale-50  lg:scale-100 -translate-y-5 lg:translate-y-0 ${styles.loader} z-[1000]`}
     >
       <img src="./favicons/small-icon.png" alt="icon" />
 
