56fb7645774d6a48471d6b62128b2aa69a71b029
mikelisrael <israelipinkz@gmail.com>
2023-03-20 15:13:03 +0100

    style: switch to css fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-});
-
-
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <main className={poppins.className}>
-        <Component {...pageProps} />
-      </main>
-  );
+  return <Component {...pageProps} />;
 }
