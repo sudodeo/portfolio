59d7ff29f493edbcfc6f220c8cc92efbf64d5daf
mikelisrael <israelipinkz@gmail.com>
2023-03-24 06:32:34 +0100

    wip: languages and tools tabs better display

--- components/StacksTab.tsx
+++ components/StacksTab.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Stacks = () => {
   return (
     <div className="pt-10" data-aos="zoom-out">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eius
-      nostrum nemo minus quia similique, placeat perferendis expedita dolores
-      porro rerum nisi adipisci quasi culpa ipsum tempore, impedit voluptas
-      doloremque.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, ratione,
+      aliquid atque aspernatur eos, inventore vitae quod tempora architecto odio
+      itaque maxime necessitatibus obcaecati. Minima ipsa dolorum repudiandae
+      nihil iusto!
     </div>
   );
 };
